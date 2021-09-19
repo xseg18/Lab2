8ec5e0bd8253c8892b0313ea228327e7e7e27bea
Ximena Elizardi <ximenagobern@gmail.com>
2021-09-19 11:38:35 -0600

    Inicio LZW

---
+++ Lab3/LZW.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab3
+{
+    public interface LZW
+    {
+        public string Comprimir(string compress);
+        public string Descomprimir(string decompress);
+    }
+}
