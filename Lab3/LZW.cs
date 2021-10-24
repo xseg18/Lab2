d7838b088ac5211f0518383eda49b8607a3ffdfb
Ximena Elizardi <ximenagobern@gmail.com>
2021-10-24 17:21:45 -0600

    LZW fixes compresión arreglada

--- Lab3/LZW.cs
+++ Lab3/LZW.cs
@@ -8,57 +8,61 @@ namespace Lab3
 {
     public interface ILZW
     {
-        public string Comprimir(string uncompressed);
-        public string Descomprimir(string decompress);
+        public byte[] Comprimir(byte[] uncompressed);
+        public byte[] Descomprimir(byte[] decompress);
     }
     public class ImplementationClassL : ILZW
     {
-        public string Comprimir(string uncompressed)
+        public byte[] Comprimir(byte[] uncompressed)
         {
             //atributos
             int nbytes = 0;
-            int origdic = 0;
-            string salida = "";
+            List<byte> salida = new List<byte>(); 
             List<int> comp = new List<int>();
-            string w = "";
-            Dictionary<string, int> Leyenda = new Dictionary<string, int>();
+            byte[] w = new byte[1];
+            Dictionary<byte, int> Leyenda = new Dictionary<byte, int>();
             //llenamos diccionario
             for (int i = 0; i < uncompressed.Length; i++)
             {
-                if (!Leyenda.ContainsKey(uncompressed[i].ToString()))
+                if (!Leyenda.ContainsKey(uncompressed[i]))
                 {
-                    Leyenda.Add(uncompressed[i].ToString(), Leyenda.Count + 1);
+                    Leyenda.Add(uncompressed[i], Leyenda.Count + 1);
                 }
             }
-            origdic = Leyenda.Count();
             //compresión
-            foreach(char k in uncompressed)
+            Dictionary<byte[], int> Combinaciones = new Dictionary<byte[], int>();
+            //tamaño del diccionario 
+            int copydic = Leyenda.Count;
+            foreach(byte k in uncompressed)
             {
-                string wk = w + k;
-                if (Leyenda.ContainsKey(wk))
+                byte[] wk = new byte[1];
+                wk.Concat(w);
+                wk.Append(k);
+                wk.ToArray();
+                if (Combinaciones.ContainsKey(wk))
                 {
                     w = wk;
                 }
                 else
                 {
-                    comp.Add(Leyenda[w]);
-                    Leyenda.Add(wk, Leyenda.Count + 1);
-                    w = k.ToString();
+                    comp.Add(Combinaciones[w]);
+                    Combinaciones.Add(wk, copydic++);
+                    w = null;
+                    w[0] = k;
                 }
             }
-            if (!string.IsNullOrEmpty(w))
+            if (w == null)
             {
-                comp.Add(Leyenda[w]);
+                comp.Add(Combinaciones[w]);
             }
-
             //lectura de bits 
-            nbytes = Convert.ToInt32((uint)Math.Log(Leyenda.Count, 2.0) + 1);
+            nbytes = Convert.ToInt32((uint)Math.Log(Leyenda.Count + Combinaciones.Count, 2.0) + 1);
             //composición inicial de salida
-            salida += Convert.ToChar(nbytes);
-            salida += Convert.ToChar(origdic);
-            for (int i = 1; i <= origdic; i++)
+            salida.Add(Convert.ToByte(nbytes));
+            salida.Add(Convert.ToByte(Leyenda.Count));
+            for (int i = 1; i <= Leyenda.Count; i++)
             {
-                salida += Leyenda.FirstOrDefault( x => x.Value == i ).Key;
+                salida.Add(Leyenda.FirstOrDefault( x => x.Value == i ).Key);
             }
             //de decimal a binario
             string codes = "";
@@ -77,7 +81,7 @@ namespace Lab3
                 else
                 {
                     long B = Convert.ToInt32(bfin);
-                    salida += Convert.ToChar(BinDec(B));
+                    salida.Add(Convert.ToByte(BinDec(B)));
                     bfin = codes[i].ToString();
                 }
             }
@@ -88,9 +92,9 @@ namespace Lab3
                     bfin += 0;
                 }
                 long B = Convert.ToInt32(bfin);
-                salida += Convert.ToChar(BinDec(B));
+                salida.Add(Convert.ToByte(BinDec(B)));
             }
-            return salida;
+            return salida.ToArray();
         }
 
         public string Descomprimir(string compressed)
